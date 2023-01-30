4d4d176712cfdefb1a909b20b1cac5c6bf57df58
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2023-01-29 18:48:43 -0600

    exercise #1 done

--- main.py
+++ main.py
@@ -3,14 +3,23 @@
 # Write the code that:
 # 1. Prompts the user to enter a letter in the alphabet:
 #      Please enter a letter from the alphabet (a-z or A-Z):
+
 # 2. Write the code that determines whether the letter entered is a vowel
 # 3. Print one of following messages (substituting the letter for x):
 #      - The letter x is a vowel
 #      - The letter x is a consonant
 # Hints:  Use the in operator to check if a character is in another string
 #         For example, if some_char in 'abc':
+letter = input('Enter a letter from the alphabet: ').lower()
+vowels = 'aeiou' 
+consonants = 'bcdfghjklmnpqrstvwxyz'
 
-
+if letter in vowels:
+    print(f'The letter {letter} is a vowel')
+elif letter in consonants:
+    print(f'The letter {letter} is a vowel')
+else:
+    print('quit')
 
 # exercise-02 Length of Phrase
 
